356fe710d0a4ffc512657b826ccfdb9d1d0d4ac1
pavanokok <pavan84461135@gmail.com>
2025-06-12 11:25:16 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -381,4 +381,8 @@ async def list_downloads():
                 })
     return {'files': files}
 
+
+if __name__ == "__main__":Add commentMore actions
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
  
